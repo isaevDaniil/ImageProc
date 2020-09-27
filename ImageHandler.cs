a3b9087bdcbcd16b49ab3ccb180c8c02203ae8b0
isaevDaniil <isaev.daniil48@yandex.ru>
2020-09-27 19:14:57 +0300

    Добавьте файлы проекта.

---
+++ ImageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProc
+{
+    class ImageHandler
+    {
+        public static Bitmap ChangeImageColor(Bitmap originalImg)
+        {
+            var newImage = new Bitmap(originalImg.Width, originalImg.Height);
+            for (int i = 0; i < originalImg.Width; i++)
+            {
+                for (int j = 0; j < originalImg.Height; j++)
+                {
+                    var origColor = originalImg.GetPixel(i, j);
+                    var newColorValue = ((int)origColor.R + (int)origColor.G + (int)origColor.B) / 3;
+                    var newColor = Color.FromArgb(newColorValue, newColorValue, newColorValue);
+                    newImage.SetPixel(i, j, newColor);
+                }
+            }
+            return newImage;
+        }
+
+        public static void SaveImage(Bitmap image, string path, string format)
+        {
+            string newPath = path.Remove(path.Length - 4) + "-result." + format;
+            image.Save(newPath);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path, format;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Введите путь к файлу:");
+                path = Console.ReadLine();
+            }
+            else path = args[0];
+
+            try
+            {
+                var inputImg = new Bitmap(Image.FromFile(path));
+                format = path.Substring(path.Length - 3);
+                if (!(format == "jpg" || format == "png" || format == "bmp"))
+                {
+                    throw new Exception("неверный формат файла");
+                }
+                var outputImg = ImageHandler.ChangeImageColor(inputImg);
+                ImageHandler.SaveImage(outputImg, path, format);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Ошибка: неверный путь к файлу\n" + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+    }
+}
